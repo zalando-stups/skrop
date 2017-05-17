9ff613c60a78212cee2d329145e7dcccc9a5620e
Roberto Gritti <roberto.gritti@zalando.de>
2017-05-17 12:31:47 +0200

    #475 adding filters and tests

---
+++ filters/quality.go
@@ -0,0 +1,56 @@
+package filters
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/zalando/skipper/filters"
+	"gopkg.in/h2non/bimg.v1"
+)
+
+const (
+	QualityName = "quality"
+)
+
+type quality struct {
+	percentage int
+}
+
+func NewQuality() filters.Spec {
+	return &quality{}
+}
+
+func (r *quality) Name() string {
+	return QualityName
+}
+
+func (r *quality) CreateOptions(image *bimg.Image) (*bimg.Options, error) {
+	log.Debug("Create options for quality ", r)
+
+	return &bimg.Options{
+		Quality: r.percentage}, nil
+}
+
+func (r *quality) CreateFilter(args []interface{}) (filters.Filter, error) {
+	var err error
+
+	if len(args) != 1 {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+
+	f := &quality{}
+
+	f.percentage, err = parseEskipIntArg(args[0])
+
+	if err != nil {
+		return nil, err
+	} else if f.percentage > 100 {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+
+	return f, nil
+}
+
+func (r *quality) Request(ctx filters.FilterContext) {}
+
+func (r *quality) Response(ctx filters.FilterContext) {
+	handleResponse(ctx, r)
+}
---
+++ filters/resizebywidth.go
@@ -0,0 +1,54 @@
+package filters
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/zalando/skipper/filters"
+	"gopkg.in/h2non/bimg.v1"
+)
+
+const (
+	ResizeByWidthName = "width"
+)
+
+type resizeByWidth struct {
+	width int
+}
+
+func NewResizeByWidth() filters.Spec {
+	return &resizeByWidth{}
+}
+
+func (r *resizeByWidth) Name() string {
+	return ResizeByWidthName
+}
+
+func (r *resizeByWidth) CreateOptions(image *bimg.Image) (*bimg.Options, error) {
+	log.Debug("Create options for resize by width ", r)
+
+	return &bimg.Options{
+		Width: r.width}, nil
+}
+
+func (r *resizeByWidth) CreateFilter(args []interface{}) (filters.Filter, error) {
+	var err error
+
+	if len(args) != 1 {
+		return nil, filters.ErrInvalidFilterParameters
+	}
+
+	f := &resizeByWidth{}
+
+	f.width, err = parseEskipIntArg(args[0])
+
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+func (r *resizeByWidth) Request(ctx filters.FilterContext) {}
+
+func (r *resizeByWidth) Response(ctx filters.FilterContext) {
+	handleResponse(ctx, r)
+}
