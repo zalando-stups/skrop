681490eb176ce0f7a2fc8a538153a10de6ae4f38
Roberto Gritti <roberto.gritti@zalando.de>
2017-08-04 13:49:22 +0200

    adding custom data client to skipper configuration

--- cmd/skrop/main.go
+++ cmd/skrop/main.go
@@ -9,7 +9,9 @@ import (
 	"os"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/zalando-incubator/skrop"
 	"github.com/zalando/skipper/proxy"
+	"github.com/zalando/skipper/routing"
 )
 
 const (
@@ -99,8 +101,10 @@ func main() {
 	log.Debug(fmt.Sprintf("Using routes-file %s", routesFile))
 
 	o := skipper.Options{
-		Address:    address,
-		RoutesFile: routesFile,
+		Address: address,
+		CustomDataClients: []routing.DataClient{
+			skrop.NewSkropDataClient(routesFile),
+		},
 		CustomFilters: []filters.Spec{
 			skropFilters.NewResize(),
 			skropFilters.NewCrop(),
---
+++ dataclient.go
@@ -0,0 +1,59 @@
+package skrop
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/zalando/skipper/eskip"
+	"github.com/zalando/skipper/eskipfile"
+	"github.com/zalando/skipper/routing"
+)
+
+type skropDataClient struct {
+	fileName string
+	prepend  *eskip.Filter
+	append   *eskip.Filter
+}
+
+func NewSkropDataClient(eskipFile string) routing.DataClient {
+
+	emptyArgs := make([]interface{}, 0)
+
+	pre := &eskip.Filter{
+		Name: "finalizeResponse",
+		Args: emptyArgs,
+	}
+	post := &eskip.Filter{
+		Name: "setupResponse",
+		Args: emptyArgs,
+	}
+	return skropDataClient{
+		fileName: eskipFile,
+		prepend:  pre,
+		append:   post,
+	}
+}
+
+func (s skropDataClient) LoadAll() ([]*eskip.Route, error) {
+	f, err := eskipfile.Open(s.fileName)
+	if err != nil {
+		log.Error("Error while opening eskip file", err)
+		return nil, err
+	}
+
+	routes, err := f.LoadAll()
+	if err != nil {
+		log.Error("Error while loading eskip routes", err)
+		return nil, err
+	}
+
+	for _, route := range routes {
+		if route.BackendType != eskip.ShuntBackend {
+			route.Filters = append(append([]*eskip.Filter{s.prepend}, route.Filters...), s.append)
+		}
+	}
+
+	return routes, nil
+}
+
+func (s skropDataClient) LoadUpdate() ([]*eskip.Route, []string, error) {
+	return nil, nil, nil
+}
