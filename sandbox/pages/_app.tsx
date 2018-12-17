07c084badd5c4bfe8ab1e5940884d85c5c0878f3
Gleb Galkin <artdaw@gmail.com>
2018-12-17 19:10:30 +0100

    feat(skeleton): skeleton of the project added Closes #144

--- sandbox/pages/_app.tsx
+++ sandbox/pages/_app.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import App, { Container } from "next/app";
 import NextSeo from "next-seo";
 import SEO from "../next-seo.config";
+import PageMenu from "ui-components/menu/menu";
 import "../skrop_theme/semantic.less";
+import { string } from "prop-types";
 
 export default class SkropApp extends App {
   public static async getInitialProps({ Component, router, ctx }) {
@@ -15,7 +17,12 @@ export default class SkropApp extends App {
     return { pageProps };
   }
 
-  public state = { error: null, errorInfo: null };
+  public state = {
+    error: null,
+    errorInfo: null,
+    filters: Array,
+    selectedImageUrl: string
+  };
 
   public componentDidCatch(error, errorInfo) {
     this.setState({
@@ -26,14 +33,22 @@ export default class SkropApp extends App {
 
   public render() {
     const { Component, pageProps } = this.props;
+    const propagatedProps = {
+      onImageSelect: this.selectImage
+    };
 
     return (
       <Container>
-        <div className="page">
+        <PageMenu logoSrc={require("static/favicon/apple-touch-icon.png")} />
+        <div className="page" style={{ marginTop: "7rem" }}>
           <NextSeo config={SEO} />
-          <Component {...pageProps} />
+          <Component {...pageProps} {...this.state} {...propagatedProps} />
         </div>
       </Container>
     );
   }
+
+  private selectImage = selectedImageUrl => {
+    this.setState({ selectedImageUrl });
+  };
 }
--- sandbox/pages/index/index.tsx
+++ sandbox/pages/index/index.tsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { Container, Grid, Header, Button } from "semantic-ui-react";
+import Router from "next/router";
+import { Container, Header, Button } from "semantic-ui-react";
 
-const MainPage: React.SFC = () => (
-  <Container>
-    <Grid container>
-      <Grid.Row columns="1">
-        <Grid.Column>
-          <Header as="h1">Skrop</Header>
-          <Button primary>Hello</Button>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </Container>
+export interface IMainPage {
+  onImageSelect: (imageUrl: string) => void;
+}
+
+const MainPage: React.SFC<IMainPage> = ({ onImageSelect }) => (
+  <div>
+    <Container>
+      <Header as="h1" textAlign="center">
+        Select Image
+      </Header>
+      <Button
+        onClick={() => {
+          onImageSelect(
+            "https://images.unsplash.com/photo-1544868501-b2f493d76e35?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          );
+          Router.push("/artboard");
+        }}
+        primary
+      >
+        Simulate Image Select
+      </Button>
+    </Container>
+  </div>
 );
-
 export default MainPage;
