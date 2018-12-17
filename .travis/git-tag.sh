41a9b172b0a07628af9eed4c552ea6d1e77125c0
Joel Andersson <joel.sebastian.andersson@zalando.de>
2018-12-17 16:49:42 +0100

    Fixes version, calling script instead of passing (empty) parameter

--- .travis/git-tag.sh
+++ .travis/git-tag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=$1
+. ../docker/version.sh
 
 echo "TRAVIS_BRANCH=${TRAVIS_BRANCH}"
 echo "TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST}"
@@ -10,8 +10,8 @@ if [ "${TRAVIS_BRANCH}_${TRAVIS_PULL_REQUEST}" = "master_false" ]; then
   echo "Merge to 'master'. Tagging patch version up."
   git config --global user.email "builds@travis-ci.com"
   git config --global user.name "Travis CI"
-  echo "Creating tag for version: ${VERSION}"
-  git tag ${VERSION} -a -m "Generated tag from TravisCI for build ${TRAVIS_BUILD_NUMBER}"
+  echo "Creating tag for version: ${NEXT_PATCH_VERSION}"
+  git tag ${NEXT_PATCH_VERSION} -a -m "Generated tag from TravisCI for build ${TRAVIS_BUILD_NUMBER}"
   git push -q --tags https://$GITHUB_AUTH@github.com/zalando-stups/skrop
 else
   echo "Not a merge to 'master'. Don't release a new version."
