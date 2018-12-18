6c46f840ca071fb1133f40947517ac1ec4aac411
Joel Andersson <joel.sebastian.andersson@zalando.de>
2018-12-18 16:06:46 +0100

    Revert "WIP: 138 sandbox feature branch (#157)"
    This reverts commit 4e26c32cc148d6f7bfa3837fdec06d95f332effe.

--- sandbox/next.base.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  distDir: "dist",
-  useFileSystemPublicRoutes: true,
-  publicRuntimeConfig: {
-    UNSPLASH_APP_ID: process.env.UNSPLASH_APP_ID || "",
-    UNSPLASH_APP_SECRET: process.env.UNSPLASH_APP_SECRET || ""
-  }
-};
--- sandbox/scripts/docker-stop.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker rm $(docker stop $(docker ps -a -q --filter="label=artifactId=skrop-sandbox"))
-echo 'container removed'
